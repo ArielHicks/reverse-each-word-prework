0c63537dad7f96bc0d9b693f24680380b8ca99b6
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-31 23:14:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,5 @@
-def reverse_each_word(string)
-  reverse = ""
-  index = 0
-  while index < string.length
-    reverse = string[index] + reverse
-    index += 1
-  end
-  return reverse
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
 end
